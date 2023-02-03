ecca20cd459581370b54b9614d2b28c9cd785e8d
singak1 <a97sin@gmail.com>
2023-02-03 13:26:52 -0500

    Add and test comments cell

---
+++ web/src/components/CommentsCell/CommentsCell.mock.js
@@ -0,0 +1,15 @@
+// Define your own mock data here:
+export const standard = (/* vars, { ctx, req } */) => ({
+  comments: [
+    { id: 1 ,
+      name: 'xQc',
+      body: 'GOOD ONE DUDE, OMEGALUL',
+      createdAt: '2023-02-03T18:30:59Z'
+    },
+    { id: 2,
+      name: 'Forsen',
+      body: 'What is this? a comment for four dimensional beings',
+      createdAt: '2023-02-03T18:33:00Z'
+    },
+  ],
+})
