97d0f04fc10dfabe194ac1a18733c604b7bc555c
singak1 <a97sin@gmail.com>
2023-02-03 13:56:09 -0500

    Add comments functionality to the backend and testing

---
+++ api/src/services/comments/comments.js
@@ -0,0 +1,17 @@
+import { db } from 'src/lib/db'
+
+export const comments = () => {
+  return db.comment.findMany()
+}
+
+export const comment = ({ id }) => {
+  return db.comment.findUnique({
+    where: { id },
+  })
+}
+
+export const Comment = {
+  post: (_obj, { root }) => {
+    return db.comment.findUnique({ where: { id: root?.id } }).post()
+  },
+}
