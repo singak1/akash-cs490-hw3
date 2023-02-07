0096b4885e1c7d49909fc53d13cc124aef56a346
singak1 <a97sin@gmail.com>
2023-02-07 17:11:30 -0500

    Finishing hw-3

--- api/src/services/posts/posts.test.js
+++ api/src/services/posts/posts.test.js
@@ -1,41 +1,14 @@
-import { posts, post, createPost, updatePost, deletePost } from './posts'
+import { db } from 'src/lib/db'
 
-describe('posts', () => {
-  scenario('returns all posts', async (scenario) => {
-    const result = await posts()
+export const posts = () => {
+  return db.post.findMany()
+}
 
-    expect(result.length).toEqual(Object.keys(scenario.post).length)
-  })
+export const post = ({ id }) => {
+  return db.post.findUnique({ where: { id } })
+}
 
-  scenario('returns a single post', async (scenario) => {
-    const result = await post({ id: scenario.post.one.id })
-
-    expect(result).toEqual(scenario.post.one)
-  })
-
-  scenario('creates a post', async () => {
-    const result = await createPost({
-      input: { title: 'String', body: 'String' },
-    })
-
-    expect(result.title).toEqual('String')
-    expect(result.body).toEqual('String')
-  })
-
-  scenario('updates a post', async (scenario) => {
-    const original = await post({ id: scenario.post.one.id })
-    const result = await updatePost({
-      id: original.id,
-      input: { title: 'String2' },
-    })
-
-    expect(result.title).toEqual('String2')
-  })
-
-  scenario('deletes a post', async (scenario) => {
-    const original = await deletePost({ id: scenario.post.one.id })
-    const result = await post({ id: original.id })
-
-    expect(result).toEqual(null)
-  })
-})
+export const Post = {
+  user: (_obj, { root }) =>
+    db.post.findFirst({ where: { id: root.id } }).user(),
+}
